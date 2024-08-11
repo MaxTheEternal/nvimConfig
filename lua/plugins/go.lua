ae1c3b27d582af984470856154d65583e538901a
MaxTheEternal <98349129+MaxTheEternal@users.noreply.github.com>
2024-08-11 17:20:16 +0200

    Go cli Commands

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,14 +1,17 @@
 return {
-  "ray-x/go.nvim",
-  dependencies = {  -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    require("go").setup()
-  end,
-  event = {"CmdlineEnter"},
-  ft = {"go", 'gomod'},
-  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+	"ray-x/go.nvim",
+	dependencies = { -- optional packages
+		"ray-x/guihua.lua",
+		"neovim/nvim-lspconfig",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	config = function()
+		require("go").setup()
+		require("go.format").goimports() -- goimports + gofmt
+		vim.api.nvim_create_user_command("GoTestCover", "!go test -cover", { bang = true })
+		vim.api.nvim_create_user_command("GoTestBenchmark", "!go test -bench=.", { bang = true })
+	end,
+	event = { "CmdlineEnter" },
+	ft = { "go", "gomod" },
+	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
