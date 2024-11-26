6d10c17bc6e64d7b85559aeea3851d95e059de17
MaxTheEternal <98349129+MaxTheEternal@users.noreply.github.com>
2024-11-26 23:41:42 +0100

    remove error message and fix autocomplete

--- lua/shortcuts.lua
+++ lua/shortcuts.lua
@@ -2,6 +2,8 @@ return {
 
 	{},
 	config = function()
-		vim.keymap.set("n", "<leader>cd", vim.cmd("cd %:p:h"), { desc = "[C]hange Working [D]irecorty" })
+		vim.keymap.set("n", "<leader>cd", function()
+			vim.cmd("cd %:p:h")
+		end, { desc = "[C]hange Working [D]irecorty" })
 	end,
 }
